07c0cb4d45c2ae3e3b3e1001e7a08a68a32d5ffd
Sharain7 <sharainlmc7@gmail.com>
2024-02-01 23:26:05 +0530

    implemented rough signup screen design

---
+++ app/src/main/java/com/example/plexushospital/MobilePinVerificationActivity.java
@@ -0,0 +1,14 @@
+package com.example.plexushospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MobilePinVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_pin_verification);
+    }
+}
